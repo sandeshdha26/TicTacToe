d10f0389ba0232b42cedd1d53b3d9d06bd2f8ae1
sandeshdha26 <sandeshdha26@gmail.com>
2022-01-29 23:04:02 +0530

    added function for user input and user turn

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 using namespace std;
+int numberChoice;
+char marking = 'X';
+int row, column;
 
-int printTable()
+void printTable()       //print 3x3 matrix first
 {
     int table[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
     cout<<"\t    |     |    \n";
@@ -15,11 +18,55 @@ int printTable()
     cout<<"\t    |     |    \n";
 }
 
+void userInput()
+{
+    if(marking == 'X')
+        cout<<"\nPlayer [1] turn..."<<endl;
+    else if (marking == '0')
+        cout<<"\nPlayer [2] turn..."<<endl;
+    cout<<"Enter any number from 1 to 9: ";
+    cin>>numberChoice;
 
-    /*cout<<"\t    |    |    \n";
-    cout<<"\t____|____|____\n";
-    cout<<"\t    |    |    \n";
-    cout<<"\t____|____|____\n";
-    cout<<"\t    |    |    \n";
-    cout<<"\t    |    |    \n";
-}*/
+    switch(numberChoice)
+    {
+        case 1:
+            row = 0;
+            column = 0;
+            break;
+        case 2:
+            row = 0;
+            column = 1;
+            break;
+        case 3:
+            row = 0;
+            column = 3;
+            break;
+        case 4:
+            row = 1;
+            column = 0;
+            break;
+        case 5:
+            row = 1;
+            column = 1;
+            break;
+        case 6:
+            row = 1;
+            column = 2;
+            break;
+        case 7:
+            row = 2;
+            column = 0;
+            break;
+        case 8:
+            row = 2;
+            column = 1;
+            break;
+        case 9:
+            row = 2;
+            column = 2;
+            break;
+        default:
+            cout<<" Invalid entry!!!";
+    }
+
+}
