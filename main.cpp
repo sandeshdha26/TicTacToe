40de92a638b5eb3a4496d360916033ff16ac32af
sandeshdha26 <sandeshdha26@gmail.com>
2022-02-01 16:24:06 +0530

    tictactoe game

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
+#include<unistd.h>
+
 using namespace std;
+
 int numberChoice;
 char marking = 'X';
 int row, column;
+char table[3][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
+bool draw = false;
 
 void printTable()       //print 3x3 matrix first
 {
-    int table[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
     cout<<"\t    |     |    \n";
     cout<<"\t "<<table[0][0]<<"  |  "<<table[0][1]<<"  |  "<<table[0][2]<<endl;
     cout<<"\t____|_____|____\n";
@@ -16,19 +20,22 @@ void printTable()       //print 3x3 matrix first
     cout<<"\t    |     |    \n";
     cout<<"\t "<<table[2][0]<<"  |  "<<table[2][1]<<"  |  "<<table[2][2]<<endl;
     cout<<"\t    |     |    \n";
+    cout<<"\n";
 }
 
-void userInput()
+void userInput()    //to determine user's turn as well as input from the user
 {
     if(marking == 'X')
-        cout<<"\nPlayer [1] turn..."<<endl;
-    else if (marking == '0')
-        cout<<"\nPlayer [2] turn..."<<endl;
+        cout<<"\nPlayer [1] ('X') turn..."<<endl;
+    else if (marking == 'O')
+        cout<<"\nPlayer [2] ('O') turn..."<<endl;
     cout<<"Enter any number from 1 to 9: ";
     cin>>numberChoice;
 
-    switch(numberChoice)
+    switch(numberChoice)    //convert user given single digit to row and columns
     {
+        case 0:
+            exit(1);
         case 1:
             row = 0;
             column = 0;
@@ -39,7 +46,7 @@ void userInput()
             break;
         case 3:
             row = 0;
-            column = 3;
+            column = 2;
             break;
         case 4:
             row = 1;
@@ -67,6 +74,72 @@ void userInput()
             break;
         default:
             cout<<" Invalid entry!!!";
+    }
+
+    /*check if user defined cell is already acquired or not...*/
+    if(marking == 'X' && table[row][column] != 'X' && table[row][column] != '0')
+    {
+        table[row][column] = 'X';
+        marking = 'O';
+    }
+    else if (marking == 'O' && table[row][column] != 'X' && table[row][column] != 'O')
+    {
+        table[row][column] = 'O';
+        marking = 'X';
+    }
+    else
+    {
+        cout<<"\nThis cell is already acquired. Try another cell."<<endl;
+        userInput();
+    }
+    //printTable();
+}
+
+bool gameOver()  //condition(function) to decide who wins the game
+{
+    for(int i = 0;i < 3; i++)
+    /* to check linear row and column for winning condition [row][column] */
+    if(table[i][0] == table[i][1] && table[i][0] == table[i][2] || table[0][i] == table[1][i] && table[0][i] == table[2][i])
+    return false;
+    /*to check diagonally for win condition */
+    if(table[0][0] == table[1][1] && table[0][0] == table[2][2] || table[0][2] == table[1][1] && table[0][2] == table[2][0])
+    return false;
+    //to check whether the game is in continue status or whatever the hell it means..
+    for(int i = 0;i < 3; i++)
+    for(int j = 0;j < 3; j++)
+    if(table[i][j] != 'X' && table[i][j] != 'O')
+        return true;
+
+    draw = true;
+    return false;    
+}
+
+int main()
+{
+    cout<<"\n----TIC-TAC-TOE GAME----"<<endl;
+    cout<<"--------For 2 Players--------"<<endl;
+    cout<<"Enter 0 to exit."<<endl;
+    while(gameOver())
+    {
+        printTable();
+        userInput();
+        sleep(2);
+        gameOver();
+    }
+    if(marking == 'X' && draw == false)
+    {
+        cout<<"Congratulations!!"<<endl;
+        cout<<"Player 1 has won the game..."<<endl;
+    }
+    else if (marking == 'O' && draw == false)
+    {
+        cout<<"Congratulations!!"<<endl;
+        cout<<"Player 2 has won the game..."<<endl;
+    }
+    else
+    {
+        cout<<"\nOh no!!"<<endl; 
+        cout<<"The game is draw..."<<endl;
     }
 
 }
